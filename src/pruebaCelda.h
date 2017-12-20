ea51d545636bac023e234abb4a5585d8fe96542d
jgarciapft <jgarciapft@alumnos.unex.es>
2017-12-20 18:58:46 +0100

    Añadido el fichero de cabeceras de la clase manejadora de las pruebas del TAD Celda (fichero pruebaCelda.h)

---
+++ src/pruebaCelda.h
@@ -0,0 +1,32 @@
+/*
+ * pruebaCelda.h
+ * Autor: jgarciapft
+ *
+ * Descripción: Clase que maneja las pruebas sobre el TAD Celda
+ *
+ */
+
+#ifndef PRUEBACELDA_H_
+#define PRUEBACELDA_H_
+
+/*
+ * Verifica que las celdas se inicializan correctamente
+ */
+void pruebaIniciar();
+
+/*
+ * Verifica que funcionen correctamente los metodos ponerValor(), obtenerValor() y vaciarCelda()
+ */
+void pruebaValorCelda();
+
+/*
+ * Verifica que funcione correctamente el método sonIguales()
+ */
+void pruebaSonIguales();
+
+/*
+ * Módulo que llama a todas las pruebas
+ */
+void pruebasTADCelda();
+
+#endif /* PRUEBACELDA_H_ */
