f62907186bb5120499da1130781d93e402a194b8
jgarciapft <jgarciapft@alumnos.unex.es>
2017-12-21 13:01:21 +0100

    Añadidas las pre/post condiciones y la complejidad a las cabeceras de los métodos. Renombrados los métodos: insertarCelda() -> insertarFicha() eliminarCelda() -> eliminarFicha() celdaVoltear()	-> fichaVoltear() cabeNuevaFila()	-> cabeFila() insertarNuevaFila() -> insertarFila()

--- src/Tablero.h
+++ src/Tablero.h
@@ -18,40 +18,90 @@ struct Tablero{
 	int tamanoTablero;
 };
 
-
+/*
+ * PRE: Existe el fichero de configuración AngelMart.cnf en la raiz y es correcto
+ * POST: Devuelve un entero indicando el tamaño del tablero indicado por la configuración
+ * COMPLEJIDAD: O(1)
+ */
 int obtenerConfTamanoTablero();
 
-
+/*
+ * PRE: 4 <= 'tamano' <= 10
+ * POST: Modifica el Tablero 't' para iniciarlo con el tamño indicado por 'tamano'
+ * COMPLEJIDAD: 0(1)
+ */
 void iniciarTablero(Tablero &t, int tamano);
 
+/*
+ * PRE: 'pos_x' y 'pos_y' dentro de los límites del Tablero 't' y la celda objetivo está vacia
+ * POST: Modifica el Tablero 't' para insertar una ficha en la posición ('pos_x', 'pos_y')
+ * COMPLEJIDAD: O(1)
+ */
+void insertarFicha(Tablero &t, int pos_x, int pos_y);
 
-void insertarCelda(Tablero &t, int pos_x, int pos_y);
+/*
+ * PRE: 'pos_x' y 'pos_y' dentro de los límites del Tablero 't' y la celda objetivo contiene una ficha
+ * POST: Modifica el Tablero 't' para eliminar la ficha en la posición ('pos_x', 'pos_y')
+ * COMPLEJIDAD: O(1)
+ */
+void eliminarFicha(Tablero &t, int pos_x, int pos_y);
 
+/*
+ * PRE: 'pos_x' y 'pos_y' dentro de los límites del Tablero 't' y la celda objetivo contiene una ficha
+ * POST: Modifica el Tablero 't' para voltear (mostrar el anverso si muestra el reverso y viceversa) una ficha
+ * COMPLEJIDAD: O(1)
+ */
+void fichaVoltear(Tablero &t, int pos_x, int pos_y);
 
-void eliminarCelda(Tablero &t, int pos_x, int pos_y);
+/*
+ * PRE:
+ * POST: Devuelve si cabe una nueva Fila en el Tablero 't'
+ * COMPLEJIDAD: O(1)
+ */
+bool cabeFila(Tablero t);
 
+/*
+ * PRE: cabe una nueva fila en el Tablero 't'
+ * POST: Modifica el Tablero 't' para insertar una nueva fila de fichas
+ * 			en la parte superior y que bajen hasta ocupar los huecos libres del tablero
+ * COMPLEJIDAD: O(n)
+ */
+void insertarFila(Tablero &t);
 
-bool cabeNuevaFila(Tablero t);
-
-
-void insertarNuevaFila(Tablero &t);
-
-
+/*
+ * PRE:
+ * POST: Devuelve si el Tablero 't' está vacio
+ * COMPLEJIDAD: 0(1)
+ */
 bool estaVacio(Tablero t);
 
-
+/*
+ * PRE:
+ * POST: Devuelve el tamaño real indicado por el fichero de configuracion del Tablero 't'
+ * COMPLEJIDAD: 0(1)
+ */
 int obtenerTamanoTablero(Tablero t);
 
-
+/*
+ * PRE: 'pos_x' y 'pos_y' dentro de los límites del Tablero 't'
+ * POST: Devuelve si la celda de la posicion ('pos_x','pos_y') contiene o no una ficha
+ * COMPLEJIDAD: 0(1)
+ */
 bool celdaEstaVacia(Tablero t, int pos_x, int pos_y);
 
-
+/*
+ * PRE: 'pos_x' y 'pos_y' dentro de los límites del Tablero 't' y existe una ficha en la posicion ('pos_x','pos_y')
+ * POST: Devuelve el valor de la celda de la posicion ('pos_x','pos_y') contiene o no una ficha
+ * COMPLEJIDAD: 0(1)
+ */
 int celdaObtenerValor(Tablero t, int pos_x, int pos_y);
 
-
+/*
+ * PRE: 'pos_x' y 'pos_y' dentro de los límites del Tablero 't' y existe una ficha en en la posicion ('pos_x','pos_y')
+ * POST: Devuelve si la celda de la posicion ('pos_x','pos_y') esta mostrando el anverso
+ * 			(el número es visible) o no
+ * COMPLEJIDAD: 0(1)
+ */
 bool celdaObtenerMostrandoAnverso(Tablero t, int pos_x, int pos_y);
-
-
-void celdaVoltear(Tablero &t, int pos_x, int pos_y);
 
 #endif /* TABLERO_H_ */
