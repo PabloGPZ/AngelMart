e917e0c82f0d1c2c5283b6d5dfcb94517bbc8d64
jgarciapft <jgarciapft@alumnos.unex.es>
2017-12-28 15:57:04 +0100

    Añadidos los archivos 'pruebaTablero.cpp' y 'pruebaTablero.h' para manejar las pruebas del TAD Tablero. Falta implementar los métodos

---
+++ src/pruebaTablero.cpp
@@ -0,0 +1,37 @@
+/*
+ * pruebaTablero.cpp
+ * Autor: jgarciapft
+ *
+ * Descripción: Clase que maneja las pruebas sobre el TAD Tablero
+ *
+ */
+
+#include "pruebaTablero.h"
+#include "Tablero.h"
+#include <iostream>
+
+using namespace std;
+
+void pruebaIniciarTablero() {
+}
+
+void pruebaInsertarFicha() {
+}
+
+void pruebaEliminarFicha() {
+}
+
+void pruebaFichaVoltear() {
+}
+
+void pruebaCabeFila() {
+}
+
+void pruebaInsertarFila() {
+}
+
+void pruebaEstaVacio() {
+}
+
+void pruebaGenerarVectorUnico() {
+}
