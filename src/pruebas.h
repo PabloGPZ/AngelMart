036e25278ed976e828f0c1e3396a05de3ba3d857
jgarciapft <jgarciapft@alumnos.unex.es>
2018-01-03 12:56:09 +0100

    Añadido el fichero de cabeceras de 'pruebas' para que en un solo módulo se llamen a todas las pruebas de todos los TAD

---
+++ src/pruebas.h
@@ -0,0 +1,21 @@
+/*
+ * pruebas.h
+ *
+ * Definición: Archivo de cabeceras para incluir un módulo que realice todas las pruebas
+ */
+
+#ifndef PRUEBAS_H_
+#define PRUEBAS_H_
+
+/*
+ * Módulo responsable de las pruebas del entorno gráfico
+ */
+void pruebasEntorno();
+
+/*
+ * Módulo responsable de llamar a todas las pruebas de todos los TAD que requieren
+ * de pruebas
+ */
+void prubebas();
+
+#endif /* PRUEBAS_H_ */
