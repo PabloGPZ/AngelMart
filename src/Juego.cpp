7dde4c8f418c302f19d8d392cc0320736841a1c2
jgarciapft <jgarciapft@alumnos.unex.es>
2018-06-27 15:21:21 +0200

    Implementada una nueva estructura para el TAD Juego. Modificados los módulos implementados e introducidos los getters de la nueva estructura.
    Modificado el código anterior para eliminar las variables globales y acomodar la nueva estructura
    
    AVISO: Investigar un bug relacionado con actualizar el entorno tras eliminar fichas de posiciones intermedias

--- src/Juego.cpp
+++ src/Juego.cpp
@@ -8,50 +8,48 @@
 
 using namespace std;
 
-int tamanoTablero;  												//Guardará el número de filas y columnas del tablero, rol: valor fijo
-int tiempoJugada;   												//Guardará el tiempo de una jugada, rol: valor fijo
-int puntosPista; 													//Guardara el número de puntos que se restan en algunas ampliaciones, rol: valor fijo
-int seg;	  														//Almacena los segundos del cronometro, rol: más reciente
-int puntuacion;														//Almacena la puntuación, rol: transformación
-string mensaje;  													//Almacena los diferentes mensajes que aparecen durante la ejecucion, rol: valor fijo
-bool salir;															//Controla la salida del bucle, rol: bandera
-TipoTecla tecla;													//Almacena las teclas pulsadas, rol: mas reciente
-int pos_x, pos_y;													//Almacena la posición de la celda seleccionada, rol: transformación
-int sel_x1, sel_y1, sel_x2, sel_y2; 								//Almacenan las posiciones de las dos fichas seleccionadas, rol: transformación
-int seleccionadas;													//Almacena el numero de fichas seleccionadas, rol: contador
-Tablero t;															//Almacena el tablero en memoria, rol: transformación
+void cargarJuego(Juego &j){
+	int tamanoTablero;  											//Guardará el número de filas y columnas del tablero, rol: valor fijo
+	int tiempoJugada;   											//Guardará el tiempo de una jugada, rol: valor fijo
+	int puntosPista; 												//Guardara el número de puntos que se restan en algunas ampliaciones, rol: valor fijo
 
-void cargarJuego(){
 	//Carga la configuración e inicia el tablero
 	if(entornoCargarConfiguracion(tamanoTablero, tiempoJugada, puntosPista)){
 		if(entornoIniciar(tamanoTablero)){
-			iniciarTablero(t, tamanoTablero);
-			manejadorJuego();										//Inicia la partida
+			/*
+			 * INICIALIZACIÓN DEL TABLERO DE JUEGO Y PUNTUACIÓN
+			 */
+			iniciarTablero(j.t, tamanoTablero);
+			j.puntuacion = PTOS_INICALES;
+			entornoPonerPuntuacion(PTOS_INICALES);
+			j.tiempoJugada = tiempoJugada;
+			j.puntosPista = puntosPista;
+			
+			manejadorJuego(j);										//Inicia la partida
 		}
 	}
 }
 
-void manejadorJuego(){
+void manejadorJuego(Juego &j){
+	int seg = 0;	  												//Almacena los segundos del cronometro, rol: más reciente
+	bool salir = false;												//Controla la salida del bucle, rol: bandera
+	TipoTecla tecla;												//Almacena las teclas pulsadas, rol: mas reciente
+	int pos_x, pos_y;												//Almacena la posición de la celda seleccionada, rol: transformación
+	int sel_x1, sel_y1, sel_x2, sel_y2; 							//Almacenan las posiciones de las dos fichas seleccionadas, rol: transformación
+	int seleccionadas = 0;											//Almacena el numero de fichas seleccionadas, rol: contador
+
 	/*
-	 * INICIALIZACIÓN
+	 * INICIALIZACIÓN DEL ENTORNO
 	 */
 	pos_x = 0;
-	pos_y = tamanoTablero-1;
+	pos_y = obtenerTamanoTablero(obtenerTablero(j))-1;
 	entornoActivarCelda(pos_y, pos_x);
-
-	puntuacion = 0;
-	entornoPonerPuntuacion(puntuacion);
-
-	salir = false;
-
-	seleccionadas = 0;
-	seg = 0;
 
 	//Inserción de filas iniciales
 	for(int i=0; i<FILAS_INICIALES; i++){
-		insertarFila(t, time(NULL)+i);								//El tiempo + i como semilla para que se generen 2 filas distintas en un mismo segundo
+		insertarFila(obtenerTablero(j), time(NULL)+i);				//El tiempo + i como semilla para que se generen 2 filas distintas en un mismo segundo
 	}
-	actualizarEntorno(t);
+	actualizarEntorno(j);
 
 	/*
 	 * Bucle que mantiene el procesamiento de la lógica del juego
@@ -60,11 +58,11 @@ void manejadorJuego(){
 		/*
 		 * Manejador de tiempo
 		 */
-		entornoTiempo(seg, tiempoJugada);							//Se actualizan y muestran los segundos del cronómetro
-		if(seg == tiempoJugada){									//Al llegar a 'tiempoJugada' segundos, el cronómetro se vuelve a iniciar
+		entornoTiempo(seg, obtenerTiempoJugada(j));					//Se actualizan y muestran los segundos del cronómetro
+		if(seg == obtenerTiempoJugada(j)){							//Al llegar a 'tiempoJugada' segundos, el cronómetro se vuelve a iniciar
 			seg = 0;
-			if(cabeFila(t)) 										//Comprueba si cabe una fila adicional en el tablero
-				juegoInsertarFila(t);
+			if(cabeFila(obtenerTablero(j))) 						//Comprueba si cabe una fila adicional en el tablero
+				juegoInsertarFila(j);
 			else
 				salir = true;
 		}
@@ -77,8 +75,8 @@ void manejadorJuego(){
 			case TEnter:
 				switch(seleccionadas){
 				case 0:
-					if(!celdaObtenerEstaVacia(t, pos_x, pos_y)){ 	//Se comprueba que la celda no esté vacia
-						juegoFichaVoltear(t, pos_x, pos_y);
+					if(!celdaObtenerEstaVacia(obtenerTablero(j), pos_x, pos_y)){ //Se comprueba que la celda no esté vacia
+						juegoFichaVoltear(j, pos_x, pos_y);
 
 						sel_x1 = pos_x;
 						sel_y1 = pos_y;
@@ -87,9 +85,9 @@ void manejadorJuego(){
 					}
 					break;
 				case 1:
-					if(!celdaObtenerEstaVacia(t, pos_x, pos_y)){	//Se comprueba que la celda no esté vacia
+					if(!celdaObtenerEstaVacia(obtenerTablero(j), pos_x, pos_y)){//Se comprueba que la celda no esté vacia
 						if(sel_x1 != pos_x || sel_y1 != pos_y){		//Se comprueba que no se seleccione dos veces la misma ficha
-							juegoFichaVoltear(t, pos_x, pos_y);
+							juegoFichaVoltear(j, pos_x, pos_y);
 
 							sel_x2 = pos_x;
 							sel_y2 = pos_y;
@@ -104,7 +102,7 @@ void manejadorJuego(){
 				break;
 			case TDerecha:
 				entornoDesactivarCelda(pos_y, pos_x);
-				if (pos_x < tamanoTablero-1)
+				if (pos_x < obtenerTamanoTablero(obtenerTablero(j))-1)
 					pos_x++;
 				else
 					pos_x = 0;
@@ -115,7 +113,7 @@ void manejadorJuego(){
 				if (pos_x > 0)
 					pos_x--;
 				else
-					pos_x = tamanoTablero-1;
+					pos_x = obtenerTamanoTablero(obtenerTablero(j))-1;
 				entornoActivarCelda(pos_y, pos_x);
 				break;
 			case TArriba:
@@ -123,12 +121,12 @@ void manejadorJuego(){
 				if (pos_y > 0)
 					pos_y--;
 				else
-					pos_y = tamanoTablero-1;
+					pos_y = obtenerTamanoTablero(obtenerTablero(j))-1;
 				entornoActivarCelda(pos_y, pos_x);
 				break;
 			case TAbajo:
 				entornoDesactivarCelda(pos_y, pos_x);
-				if (pos_y < tamanoTablero-1)
+				if (pos_y < obtenerTamanoTablero(obtenerTablero(j))-1)
 					pos_y++;
 				else
 					pos_y = 0;
@@ -153,28 +151,29 @@ void manejadorJuego(){
 		if(seleccionadas == 2){
 			entornoPausa(TIEMPO_RETRASO_SELECCION); 				//Pausa para que el jugador vea la comparación
 
-			if(compararFichas(t, sel_x1, sel_y1, sel_x2, sel_y2)){ 	//Son iguales
+			if(compararFichas(obtenerTablero(j), sel_x1, sel_y1, sel_x2, sel_y2)){ 	//Son iguales
 				if(sel_y1 < sel_y2){ 								//Si se elimina primero la ficha inferior la superior cae y se desplaza, por lo que su posicion varia
-					juegoEliminarFicha(t, sel_x1, sel_y1);
-					juegoEliminarFicha(t, sel_x2, sel_y2);
+					juegoEliminarFicha(j, sel_x1, sel_y1);
+					juegoEliminarFicha(j, sel_x2, sel_y2);
 				}else{
-					juegoEliminarFicha(t, sel_x2, sel_y2);
-					juegoEliminarFicha(t, sel_x1, sel_y1);
+					juegoEliminarFicha(j, sel_x2, sel_y2);
+					juegoEliminarFicha(j, sel_x1, sel_y1);
 				}
 
-				puntuacion = puntuacion + PTOS_PAREJA; 				//Se suman los puntos
-				entornoPonerPuntuacion(puntuacion);
+				obtenerPuntuacion(j) = obtenerPuntuacion(j) + PTOS_PAREJA; //Se suman los puntos
+				entornoPonerPuntuacion(obtenerPuntuacion(j));
 
 				/*
 				 * Manejador de final por vaciar tablero
 				 */
-				if(tableroEstaVacio(t))
+				if(tableroEstaVacio(obtenerTablero(j)))
 					salir = true;
-			}else{ //Son distintas
-				juegoFichaVoltear(t, sel_x1, sel_y1); 				//Se vuelven a voltear las fichas
-				juegoFichaVoltear(t, sel_x2, sel_y2);
+			}else{													//Son distintas
+				juegoFichaVoltear(j, sel_x1, sel_y1);				//Se vuelven a voltear las fichas
+				juegoFichaVoltear(j, sel_x2, sel_y2);
 			}
-			seleccionadas = 0; 										//Se reinicia el contador
+			
+			seleccionadas = 0;										//Se reinicia el contador
 		}
 	}
 
@@ -182,66 +181,84 @@ void manejadorJuego(){
 	 * Manejador de final de juego
 	 */
 	if(salir)
-		terminarJuego(); 											//Finaliza la partida
+		terminarJuego(j); 											//Finaliza la partida
 }
 
-void terminarJuego() {
-	if(tableroEstaVacio(t))
-		puntuacion = puntuacion + PTOS_FINAL_TABLERO_VACIO;			//Si la partida termina por vaciar el tablero se dan ptos extra
+void terminarJuego(Juego &j) {
+	string mensaje;  												//Almacena los diferentes mensajes que aparecen durante la ejecucion, rol: valor fijo
 
-	mensaje = "             Puntuación final : " + toString(puntuacion) + " ptos";
+	if(tableroEstaVacio(obtenerTablero(j)))
+		obtenerPuntuacion(j) = obtenerPuntuacion(j) + PTOS_FINAL_TABLERO_VACIO;	//Si la partida termina por vaciar el tablero se dan ptos extra
+
+	mensaje = "             Puntuación final : " + toString(obtenerPuntuacion(j)) + " ptos";
 	entornoMostrarMensajeFin(mensaje);
 
 	entornoPausa(TIEMPO_PAUSA_FINAL);
 	entornoTerminar();
 }
 
-void actualizarEntorno(Tablero &t){
-	for(int i=0; i<obtenerTamanoTablero(t); i++){
-		for(int j=0; j<obtenerTamanoTablero(t); j++){
-			if(!celdaObtenerEstaVacia(t, j, i)){ 					//Actualiza el estado de la ficha en función de su miembro 'mostrandoAnverso'
-				if(celdaObtenerMostrandoAnverso(t, j, i))
-					entornoFichaAnverso(i, j, celdaObtenerValor(t, j, i));
+void actualizarEntorno(Juego &j){
+	for(int i=0; i<obtenerTamanoTablero(obtenerTablero(j)); i++){
+		for(int z=0; z<obtenerTamanoTablero(obtenerTablero(j)); z++){
+			if(!celdaObtenerEstaVacia(obtenerTablero(j), z, i)){ 	//Actualiza el estado de la ficha en función de su miembro 'mostrandoAnverso'
+				if(celdaObtenerMostrandoAnverso(obtenerTablero(j), z, i))
+					entornoFichaAnverso(i, z, celdaObtenerValor(obtenerTablero(j), z, i));
 				else
-					entornoFichaReves(i, j);
-			}else if(celdaObtenerValor(t, j, i) != VALOR_PREDEFINIDO){ //Elimina las fichas que fueron borradas de la memoria del tablero
-				entornoBorrarCelda(i, j);
+					entornoFichaReves(i, z);
+			}else if(celdaObtenerValor(obtenerTablero(j), z, i) != VALOR_PREDEFINIDO){ //Elimina las fichas que fueron borradas de la memoria del tablero
+				entornoBorrarCelda(i, z);
 			}
 		}
 	}
 }
 
-void juegoInsertarFila(Tablero &t){
-	insertarFila(t, time(NULL)); 									//El timepo como semilla
+void juegoInsertarFila(Juego &j){
+	insertarFila(obtenerTablero(j), time(NULL)); 					//El timepo como semilla
 
 	/*
 	 * Hace que aparezca una fila de fichas mostrando el reverso en la parte superior del tablero
 	 * 		y después de un pequeño retraso desaparece para hacer la ilusión de que cae hacia abajo
 	 */
-	for(int i=0; i<tamanoTablero; i++){
+	for(int i=0; i<obtenerTamanoTablero(obtenerTablero(j)); i++){
 		entornoFichaReves(0, i);
 	}
 	entornoPausa(TIEMPO_INSERCION_FILA); 							//Retraso antes del borrado de la fila
-	for(int i=0; i<tamanoTablero; i++){
+	for(int i=0; i<obtenerTamanoTablero(obtenerTablero(j)); i++){
 		entornoBorrarCelda(0, i);
 	}
 
-	actualizarEntorno(t);
+	actualizarEntorno(j);
 }
 
-void juegoEliminarFicha(Tablero &t, int pos_x, int pos_y){
-	if(!celdaObtenerEstaVacia(t, pos_x, pos_y)){ 					//Si la celda esta vacia no hace nada
-		eliminarFicha(t, pos_x, pos_y);
-		actualizarEntorno(t);
+Tablero& obtenerTablero(Juego &j){
+	return j.t;
+}
+
+int& obtenerPuntuacion(Juego &j){
+	return j.puntuacion;
+}
+
+int obtenerTiempoJugada(Juego j){
+	return j.tiempoJugada;
+}
+
+int obtenerPuntosPista(Juego j){
+	return j.puntosPista;
+}
+
+void juegoEliminarFicha(Juego &j, int pos_x, int pos_y){
+	if(!celdaObtenerEstaVacia(obtenerTablero(j), pos_x, pos_y)){ 	//Si la celda esta vacia no hace nada
+		eliminarFicha(obtenerTablero(j), pos_x, pos_y);
+		actualizarEntorno(j);
 	}
 }
 
-void juegoFichaVoltear(Tablero &t, int pos_x, int pos_y){
-	if(!celdaObtenerEstaVacia(t, pos_x, pos_y)){ 					//Si la celda esta vacia no hace nada
-		if(fichaVoltear(t, pos_x, pos_y)){ 							//Animación de volteado de la ficha
+void juegoFichaVoltear(Juego &j, int pos_x, int pos_y){
+	if(!celdaObtenerEstaVacia(obtenerTablero(j), pos_x, pos_y)){ 	//Si la celda esta vacia no hace nada
+		if(fichaVoltear(obtenerTablero(j), pos_x, pos_y)){ 			//Animación de volteado de la ficha
 			entornoFichaCanto(pos_y, pos_x);
 			entornoPausa(TIEMPO_TRANSICION_ESTADOS);
-			entornoFichaAnverso(pos_y, pos_x, celdaObtenerValor(t, pos_x, pos_y));
+			entornoFichaAnverso(pos_y, pos_x, celdaObtenerValor(obtenerTablero(j), pos_x, pos_y));
 		}else{
 			entornoFichaCanto(pos_y, pos_x);
 			entornoPausa(TIEMPO_TRANSICION_ESTADOS-0.15); 			//La animación de ANVERSO a REVERSO es más corta
@@ -251,7 +268,9 @@ void juegoFichaVoltear(Tablero &t, int pos_x, int pos_y){
 }
 
 int main(){
+	Juego j;
+
 	//Llamada a todas las pruebas
 	//pruebas();
-	cargarJuego();
+	cargarJuego(j);
 }
--- src/Juego.h
+++ src/Juego.h
@@ -14,7 +14,8 @@
 #include "pruebas.h"
 #include <time.h>
 
-const int FILAS_INICIALES = 2;
+const int FILAS_INICIALES = 2;					//Número de filas de fichas al iniciar la partida
+const int PTOS_INICALES = 0;					//Puntuación inicial de la partida
 const int PTOS_PAREJA = 10; 					//Puntos obtenidos al hacer pareja
 const int PTOS_FINAL_TABLERO_VACIO = 50; 		//Puntos de bonificación por vaciar el tablero
 const float TIEMPO_INSERCION_FILA = 0.5;		//Tiempo de retraso para mostrar una nueva fila en la parte superior del tablero
@@ -22,63 +23,102 @@ const float TIEMPO_TRANSICION_ESTADOS = 0.25; 	//Retraso entre estados de la ani
 const float TIEMPO_RETRASO_SELECCION = 0.5; 	//Tiempo que se le deja al jugador ver el anverso de ambas fichas
 const float TIEMPO_PAUSA_FINAL = 2; 			//Tiempo que se le deja al jugador ver el mensaje final
 
+struct Juego{
+	Tablero t;
+	int puntuacion;
+	int tiempoJugada;
+	int puntosPista;
+};
+
 /*
  * PRE:
  * POST: Punto de entrada del programa. Carga la configuració inicial para iniciar el entorno gráfico.
  * 			Si se realiza correctamente llama al siguiente módulo 'manejadorJuego()'.
  * 	COMPLEJIDAD: O(1)
  */
-void cargarJuego();
+void cargarJuego(Juego &j);
 
 
 /*
- * PRE:
+ * PRE:	El Juego 'j' debe estar correctamente inicializado
  * POST: Módulo que maneja toda la lógica del juego: entrada del usuario, manejo del tablero,
  *  		manejo del cronómetro, de la puntuación y de la finalización de la partida.
  * 			Una vez la partida acabe se llama al módulo 'terminarJuego()'.
  * 	COMPLEJIDAD: 0(n(*n*n²)) .Por ser una composición de módulos de complejidad (n*n²)
  */
-void manejadorJuego();
+void manejadorJuego(Juego &j);
 
 
 /*
- * PRE:
+ * PRE: El Juego 'j' debe estar correctamente inicializado
  * POST: Módulo que termina el entorno gráfico y libera los recursos asociados. También da
  * 			un mensaje de despedida con la puntuación obtenida.
  * 	COMPLEJIDAD: O(1)
  */
-void terminarJuego();
+void terminarJuego(Juego &j);
 
 
 /*
- * PRE:
+ * PRE:	El juego 'j' debe estar correctamente inicializado
  * POST: Módulo para actualizar el entorno gráfico del tablero
  * COMPLEJIDAD: 0(n²)
  */
-void actualizarEntorno(Tablero &t);
+void actualizarEntorno(Juego &j);
 
 
 /*
- * PRE: Cabe una fila más en el Tablero 't'
+ * PRE:	El juego 'j' debe estar correctamente inicializado
+ * POST: Módulo para obtener el tablero en memoria que representa la partida
+ * COMPLEJIDAD: 0(1)
+ */
+Tablero& obtenerTablero(Juego &j);
+
+
+/*
+ * PRE:	El juego 'j' debe estar correctamente inicializado
+ * POST: Módulo para obtener la puntuacion de la partida
+ * COMPLEJIDAD: 0(1)
+ */
+int& obtenerPuntuacion(Juego &j);
+
+
+/*
+ * PRE:	El juego 'j' debe estar correctamente inicializado
+ * POST: Módulo para obtener el parámetro de tiempo de una jugada
+ * COMPLEJIDAD: 0(1)
+ */
+int obtenerTiempoJugada(Juego j);
+
+
+/*
+ * PRE:	El juego 'j' debe estar correctamente inicializado
+ * POST: Módulo para obtener el parámetro del multiplicador de puntos por pista
+ * COMPLEJIDAD: 0(1)
+ */
+int obtenerPuntosPista(Juego j);
+
+
+/*
+ * PRE: El Juego 'j' debe estar correctamente inicializado y Cabe una fila más en el Tablero 't'
  * POST: Módulo que insertar una fila en el tablero y actualiza el entorno grafico
  * COMPLEJIDAD: O(n*n²) .Por actualizarEntorno()
  */
-void juegoInsertarFila(Tablero &t);
+void juegoInsertarFila(Juego &j);
 
 
 /*
- * PRE: 'pos_x' y 'pos_y' dentro de los límites del Tablero t
+ * PRE: El Juego 'j' debe estar correctamente inicializado y 'pos_x' y 'pos_y' dentro de los límites del Tablero t
  * POST: Módulo que elimina la ficha de la posición ('pos_x', 'pos_y') y actualiza el entorno gráfico
  * COMPLEJIDAD: O(n*n²) .Por actualizarEntorno()
  */
-void juegoEliminarFicha(Tablero &t, int pos_x, int pos_y);
+void juegoEliminarFicha(Juego &j, int pos_x, int pos_y);
 
 
 /*
- * PRE: pos_x' y 'pos_y' dentro de los límites del Tablero t
+ * PRE: El Juego 'j' debe estar correctamente inicializado y pos_x' y 'pos_y' dentro de los límites del Tablero t
  * POST: Módulo que voltea la ficha de la posición ('pos_x', 'pos_y') y actualiza el entorno gráfico
  * COMPLEJIDAD: O(1*n²) .Por actualizarEntorno()
  */
-void juegoFichaVoltear(Tablero &t, int pos_x, int pos_y);
+void juegoFichaVoltear(Juego &j, int pos_x, int pos_y);
 
 #endif /* JUEGO_H_ */
